104e2df7fbc21fe65e8e50390b0eb18be664529e
肖金兴 <xiao-jinxing@sohu.com>
2017-08-11 16:22:53 +0800

    Initial Commit

---
+++ music/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  music
+//
+//  Created by kent on 2017/8/11.
+//  Copyright © 2017年 肖金兴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ music/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  music
+//
+//  Created by kent on 2017/8/11.
+//  Copyright © 2017年 肖金兴. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
